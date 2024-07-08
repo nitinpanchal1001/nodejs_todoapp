bd9b3664c92d54782047ad34b7d93489e1fb6196
nitinpanchal1001 <148269020+nitinpanchal1001@users.noreply.github.com>
2024-07-08 17:55:47 +0530

    initial commit

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import {getMyProfile, login, logout, register} from '../controllers/user.js';
+import { isAuthenticated } from '../middlewares/auth.js';
+const router = express.Router();
+
+
+router.post("/login" , login)
+
+router.get("/logout" , logout)
+
+router.post("/new" , register)
+
+router.get("/me" , isAuthenticated ,getMyProfile)
+
+
+
+
+
+export default router ;
